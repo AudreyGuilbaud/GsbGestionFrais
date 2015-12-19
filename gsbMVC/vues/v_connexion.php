f8f3cb96e042d31f07ba5e57115839460e3b04f2
AudreyGuilbaud <guilbaud.audrey@live.fr>
2015-12-19 22:28:32 +0100

    Modification Connexion

--- gsbMVC/vues/v_connexion.php
+++ gsbMVC/vues/v_connexion.php
@@ -1,21 +1,26 @@
-﻿<div id="contenu">
-      <h2>Identification utilisateur</h2>
+﻿<div id="connexion" class="encadreFin">
+    <div id="identConnect">
+        Identification de l'utilisateur
+    </div>
 
+    <form method="POST" action="index.php?uc=connexion&action=valideConnexion">
+       
+       
+        <div id="loginDiv">
+            Login : &nbsp;
+            <input id="login" type="text" name="login"  size="30" maxlength="45"> <br/>
+            <br/>  
+        </div>
+        
+        <div id="mdpDiv">
+            Mot de passe : &nbsp;
+            <input id="mdp"  type="password"  name="mdp" size="30" maxlength="45"><br/>
+            <br/>   
+        </div>
 
-<form method="POST" action="index.php?uc=connexion&action=valideConnexion">
-   
-    
-			<p>
-       <label for="nom">Login*</label>
-       <input id="login" type="text" name="login"  size="30" maxlength="45">
-      </p>
-			<p>
-				<label for="mdp">Mot de passe*</label>
-			  <input id="mdp"  type="password"  name="mdp" size="30" maxlength="45">
-      </p>
-         <input type="submit" value="Valider" name="valider">
-         <input type="reset" value="Annuler" name="annuler"> 
-      </p>
-</form>
+        <input type="submit" value="Valider" name="valider" id="valider" class="bouton">
+        <input type="reset" value="Annuler" name="annuler" id="annuler" class="bouton"> 
+        
+    </form>
 
 </div>
--- gsbMVC/vues/v_entete.php
+++ gsbMVC/vues/v_entete.php
@@ -5,6 +5,7 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
         <link href="./styles/menu.css" rel="stylesheet" type="text/css" />
+        <link href="./styles/connexion.css" rel="stylesheet" type="text/css" />
         <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico" />
     </head>
     <body>
