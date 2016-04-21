44d6a61d68a8b125731848ef093cfdc6b83063ed
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-04-21 19:32:55 +0200

    redimensionnement menu OK

--- gsbMVC/vues/v_entete.php
+++ gsbMVC/vues/v_entete.php
@@ -11,9 +11,9 @@
         <link href="./styles/etatFrais.css" rel="stylesheet" type="text/css" />
         <link href="./styles/recherches.css" rel="stylesheet" type="text/css" />
         <link href="./styles/fichesComptables.css" rel="stylesheet" type="text/css" />
-        
+
         <script type="text/javascript" src="./javascript/j_gestionLiens.js"></script> 
-        
+
         <script type="text/javascript" src="./bibliotheques/jquery-2.2.3.min.js"></script> 
 
         <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico" />
@@ -21,8 +21,12 @@
     <body>
 
         <div id="entete">
-            <div id="imgEntete">                 
+            <div id="imgEntete">
+                <img id="imgEnteteLogo" src="images/logoGSB.gif">
+
             </div>
-            <?php echo"Suivi du Remboursement des Frais" ?>
+            <div id="titreEntete">
+                <img id="imgEnteteTitre" src="images/titreEntete.JPG">
+            </div>
         </div>
         <div id="page">
